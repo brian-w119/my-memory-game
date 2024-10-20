a3b68f823fa7a58b0b8226f2909f3e2889317224
MyPc <brianw119@outlook.com>
2024-10-20 21:38:51 +0100

    Selected images saved to localStorage

--- memory-tester2/src/App.jsx
+++ memory-tester2/src/App.jsx
@@ -3,7 +3,7 @@ import {Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10} from "./Ima
 import initiateRandom, {img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, reShuffle} from "./random.js"
 import {useState, useEffect} from "react"
 
-const selectedImgs = [];
+let selectedImgs = [];
 //window.addEventListener("load", initiateRandom())
 
 //child component
@@ -15,23 +15,24 @@ const Cards = ({id, className, img, boxclicked, onClick}) => {
 
 //pushes the i.d. of image that has been clicked on to selectedImgs array
 const saveSelectedImgs = (e) => {
- // const [loaded, shuffleImgs] = useState(null);
   if(!selectedImgs.includes(e.target.id)){
     selectedImgs.push(e.target.id);
-    reShuffle();
+    localStorage.setItem("selectedImages", JSON.stringify(selectedImgs));
   };
-  console.log(selectedImgs);
+   console.log(selectedImgs);
+   const retrievedData = JSON.parse(localStorage.getItem("selectedImages"));
+   console.log(`retrievedData: ${retrievedData}`);
 };
 
 //parent component
 const App = () => {
-  //let ref = useRef(null);
+  const [selection, pushSelection] = useState(null);
   {window.onload = initiateRandom()};
-
-  const selectBox = () => { 
-    const [boxclicked, clickBox] = useState(false);
-    return clickBox(true);
-  }
+  {
+   // selectedImgs = [...retrievedData];
+   // console.log(`selectedImages: ${selectedImgs}`);
+  };
+  
   return(
     <div id="container">
         <Cards id="box1" img={img1()} onClick={(e) => saveSelectedImgs(e)} className="box"/>
